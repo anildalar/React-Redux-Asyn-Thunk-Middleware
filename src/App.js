deefb046401c4b5832dd159962544969757b29e9
“anildalar” <dollorinfotech@gmail.com>
2022-04-19 17:01:35 +0530

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { myAsyncAction } from '.';
 
 function App() {
+  let state = useSelector((storeObject)=>{
+    return storeObject;
+  });
+  let dispatch = useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {console.log(state.name)}
+        <h1>{state.name}</h1>
+        <button onClick={()=>{ dispatch(myAsyncAction()) }}>Change My Name </button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,67 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
 
+
+
+//Lets Build the store object
+let rootReducer = (prevState=initialState,action)=>{
+  let newState = prevState;
+  switch(action.type){
+    case 'CHANGE_TO_AKASH':
+      console.log('CHANGE_TO_AKASH');
+      return {
+        ...newState,
+        name:action.payload
+      }
+     
+      //break;
+    case 'CHANGE_TO_KIRAN':
+      console.log('CHANGE_TO_KIRAN');
+      return {
+        ...newState,
+        name:action.payload
+      }
+      //break;
+    default:
+      return {
+        ...newState
+      }
+  }
+  //return newState;
+}
+
+let actionCreator1 = ()=>{
+  return { 
+      type:'CHANGE_TO_AKASH',
+      payload:'AAKASH RAWAL'
+  }
+}
+//Async Action creator
+export const myAsyncAction = ()=>{
+    console.log('ok');
+    return (dispatch, getState)=>{
+        setTimeout(function(){
+          console.log('OKOK');
+          dispatch(actionCreator1());
+        },5000);
+    }
+} 
+
+let initialState = {
+  name:'ANIL DOLLOR'
+}
+let store  = createStore(rootReducer,applyMiddleware(thunk));
+//console.log(store);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+    
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
